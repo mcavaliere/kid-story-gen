933d8ca11777890a82910432893742f918f0454f
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2023-12-30 13:20:07 -0500

    chore: add shadcn

---
+++ src/lib/constants.ts
@@ -0,0 +1,137 @@
+export const AGE_GROUPS = [
+    {
+      "start_age": 0,
+      "end_age": 2,
+      "name": "Infant",
+      "themes": [
+        "Sensory Exploration",
+        "Basic Concepts (e.g., Shapes, Colors)",
+        "Lullabies",
+        "On the Farm",
+        "Bath Time",
+        "Peekaboo",
+        "Bedtime",
+        "Nature and Seasons"
+      ]
+    },
+    {
+      "start_age": 2,
+      "end_age": 3,
+      "name": "Toddler",
+      "themes": [
+        "All About Me",
+        "Animals",
+        "Balls",
+        "Bugs",
+        "Butterflies",
+        "Colors",
+        "Community Helpers"
+      ]
+    },
+    {
+      "start_age": 3,
+      "end_age": 5,
+      "name": "Preschool",
+      "themes": [
+        "Friendship",
+        "Family",
+        "Discovery (Learning)",
+        "Empathy",
+        "Courage",
+        "Suffering",
+        "School",
+        "Big Changes",
+        "Social Issues",
+        "Teamwork",
+        "Acceptance",
+        "Sharing",
+        "Holidays",
+        "Growing Up",
+        "Animals",
+        "Nature",
+        "Imagination",
+        "Adventure",
+        "Bedtime",
+        "Numbers",
+        "Opposites",
+        "Seasons",
+        "Shapes",
+        "Transportation"
+      ]
+    },
+    {
+      "start_age": 5,
+      "end_age": 7,
+      "name": "Kindergarten",
+      "themes": [
+        "Curiosity",
+        "Creativity",
+        "Problem Solving",
+        "Perseverance",
+        "Independence",
+        "Community",
+        "History",
+        "Science",
+        "Space",
+        "Superheroes",
+        "Fairy Tales",
+        "Magic",
+        "Wonders of the World"
+      ]
+    },
+    {
+      "start_age": 7,
+      "end_age": 9,
+      "name": "Early Elementary",
+      "themes": [
+        "Friendship",
+        "Teamwork",
+        "Self-Discovery",
+        "Diversity",
+        "Environment",
+        "Conservation",
+        "Exploration",
+        "Identity",
+        "Overcoming Challenges",
+        "Leadership",
+        "Invention",
+        "Innovation"
+      ]
+    },
+    {
+      "start_age": 9,
+      "end_age": 11,
+      "name": "Middle Elementary",
+      "themes": [
+        "Resilience",
+        "Empowerment",
+        "Global Awareness",
+        "Social Justice",
+        "Technology",
+        "Future",
+        "Identity",
+        "Coming of Age",
+        "Making a Difference",
+        "Cultural Diversity",
+        "Human Rights"
+      ]
+    },
+    {
+      "start_age": 11,
+      "end_age": 13,
+      "name": "Late Elementary",
+      "themes": [
+        "Self-Expression",
+        "Identity",
+        "Peer Relationships",
+        "Personal Growth",
+        "Transition",
+        "Decision Making",
+        "Ethics",
+        "Morality",
+        "Independence",
+        "Adolescence",
+        "Growing Pains"
+      ]
+    }
+  ]
