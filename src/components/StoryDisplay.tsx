9213af233198f31c0503d0a9100e8008be3fd831
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-01-27 14:41:10 -0500

    refactor: rearrange topics

--- src/components/StoryDisplay.tsx
+++ src/components/StoryDisplay.tsx
@@ -5,7 +5,7 @@ export type StoryContentType = {
 
 export function StoryDisplay({ title, content }: StoryContentType) {
   return (
-    <div className="whitespace-pre-wrap my-4 text-left">
+    <div className="whitespace-pre-wrap my-4 md:mt-0 text-left">
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       {content}
     </div>
