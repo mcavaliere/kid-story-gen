06511b89202f5a665fa18f3a7ba625d7634bc6c4
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-01-29 16:52:24 -0500

    feat: change art style

--- src/prompts/stabilityai/create-story-image.ts
+++ src/prompts/stabilityai/create-story-image.ts
@@ -1,3 +1,3 @@
 export const prompt = `
-Impressionist style of cartoon characters, {{characters}} enjoying themselves in a vibrant, painterly forest, reflecting a whimsical and joyful atmosphere, reminiscent of the techniques used by Claude Monet and Edgar Degas, with a cinematic composition, trending on ArtStation.
+  Impressionist style of cartoon characters, {{characters}} enjoying themselves in a vibrant, painterly forest, reflecting a whimsical and joyful atmosphere, reminiscent of the techniques used by Claude Monet and Edgar Degas, with a cinematic composition, trending on ArtStation.
 `;
