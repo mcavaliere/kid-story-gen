a934d61222fa03be2a1629190a8525f38c67dcfe
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-02-09 14:39:57 -0500

    feat: footer

--- src/components/server/Footer.tsx
+++ src/components/server/Footer.tsx
@@ -1,22 +1,25 @@
 import Link from 'next/link';
+import { EBLogo } from '../svgs/EBLogo';
 
 export function Footer() {
   return (
-    <footer className="flex flex-row justify-end w-full p-4">
-      <p>
-        Made by
-        <Link
-          href="https://mikecavaliere.com"
-          className="inline-block mx-1 font-bold"
-          target="_blank"
-        >
-          Mike Cavaliere
-        </Link>
-
-        |
-
-        
-      </p>
+    <footer className="flex flex-row items-center justify-end w-full p-4">
+      <span>Made by</span>
+      <Link
+        href="https://mikecavaliere.com"
+        className="ml-1 font-bold"
+        target="_blank"
+      >
+        Mike Cavaliere
+      </Link>
+      <span className="mx-2">|</span>
+      <Link
+        className="flex w-20 h-10"
+        href="https://echobind.com"
+        target="_blank"
+      >
+        <EBLogo />
+      </Link>
     </footer>
   );
 }
