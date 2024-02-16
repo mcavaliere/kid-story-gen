60005a1a978782f213a4efd05d20ca1a4709ca35
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-02-16 15:46:06 -0500

    feat: prevent strange eyes and faces in image generation

--- src/prompts/stabilityai/create-story-image.ts
+++ src/prompts/stabilityai/create-story-image.ts
@@ -1,3 +1,13 @@
 export const prompt = `
-  Digital painting style of cartoon characters, {{characterDescriptions}} engaging in playful antics within the following setting: {{setting}}, designed to evoke a whimsical and cheerful mood, in the vein of artists like Craig Mullins and Ryan Church, with a cinematic composition, trending on ArtStation
+  Digital painting style of cartoon characters, {{characterDescriptions}} engaging in playful antics within the following setting: {{setting}}, designed to evoke a whimsical and cheerful mood, in the vein of artists like Craig Mullins and Ryan Church, with a cinematic composition, trending on ArtStation,
+  realistic eyes
+`;
+
+export const negativePrompt = `
+text,
+Imperfect eyes
+Creepy eyes,
+weird eyes,
+deformed faces,
+unhappy facial expressions
 `;
