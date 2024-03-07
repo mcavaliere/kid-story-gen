c258b82f7c1e20e70b6dc1b7fd12bc74bea8c7eb
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-03-06 20:36:21 -0500

    feat: more negative prompts

--- src/prompts/stabilityai/create-story-image.ts
+++ src/prompts/stabilityai/create-story-image.ts
@@ -1,6 +1,7 @@
 export const prompt = `
-  Digital painting style of cartoon characters, {{characterDescriptions}} engaging in playful antics within the following setting: {{setting}}, designed to evoke a whimsical and cheerful mood, in the vein of artists like Craig Mullins and Ryan Church, with a cinematic composition, trending on ArtStation,
-  realistic eyes
+  Digital painting style of cartoon characters, {{characterDescriptions}} engaging in playful antics within the following setting: {{setting}}, designed to evoke a whimsical and cheerful mood, in the vein of artists like Craig Mullins and Ryan Church, trending on ArtStation,
+  realistic eyes,
+  muted colors
 `;
 
 export const negativePrompt = `
@@ -9,5 +10,6 @@ Imperfect eyes
 Creepy eyes,
 weird eyes,
 deformed faces,
-unhappy facial expressions
+unhappy facial expressions,
+surrealist style
 `;
